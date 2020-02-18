45797365eadc484c842c0d38c50af3bcb649531f
sergey-prrikhodko <sergey.prikhodko.95@gmail.com>
2020-02-18 11:24:36 +0200

    setup project

---
+++ Room Scheduler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Room Scheduler
+//
+//  Created by Sergey Prikhodko on 17.02.2020.
+//  Copyright © 2020 Sergey Prikhodko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
